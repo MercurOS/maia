1003d6a35d4b50f7254890525639268f73fa7463
Henry Carlson <henry.carlson@gmail.com>
2021-06-22 00:33:20 +0300

    Add support for EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,30 @@ use core::ffi::c_void;
 pub mod assembly;
 mod uefi;
 
-use uefi::types::EfiStatus;
+use uefi::{
+    EfiHandle,
+    EfiStatus,
+    EfiSystemTable,
+};
 
 #[no_mangle]
 pub extern "C" fn relocate(
     base_address: *const c_void,
     efi_dyn: *const c_void,
-) -> usize {
-    // EFI_LOAD_ERROR
-    0x8000_0000_0000_0001
+) -> EfiStatus {
+    EfiStatus::success()
 }
 
 #[no_mangle]
-pub extern "C" fn efi_main() -> ! {
-    loop {}
+pub extern "C" fn efi_main(
+    _image_handle: EfiHandle,
+    system_table: &'static mut EfiSystemTable,
+) -> EfiStatus {
+    system_table.console_out.write_string(
+        "/// MercurOS Maia Bootloader ///\r\n"
+    );
+
+    EfiStatus::load_error()
 }
 
 #[panic_handler]
