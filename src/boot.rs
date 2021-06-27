d3d44da9712a33894de622101d57f25620a1c25e
Henry Carlson <henry.carlson@gmail.com>
2021-06-28 01:54:10 +0300

    UEFI: Clear screen and get memory map
    UEFI API:
     - add EFI_BOOT_SERVICES::allocate_pages
     - add EFI_BOOT_SERVICES::get_memory_map
     - add EFI_BOOT_SERVICES::allocate_pool
    
    UEFI abstraction layer:
     - add UEFI::Memory trait for UEFI memory services
     - implement higher level abstraction for allocating memory and
       retrieving memory map
     - implement UEFI::Console::clear_screen

--- src/boot.rs
+++ src/boot.rs
@@ -1,10 +1,23 @@
 use super::uefi;
 
 pub fn boot(mut uefi: uefi::Application) -> uefi::EfiStatus {
+    uefi::Console::clear_screen(&mut uefi);
     uefi::Console::write_string(
         &mut uefi,
         "/// MercurOS Maia Bootloader ///\r\n"
     );
+
+    let _memory_map = match uefi::Memory::get_memory_map(&mut uefi) {
+        Ok(memory_map) => memory_map,
+        Err(uefi::UEFIError::MemoryAllocationFailed) => {
+            uefi::Console::write_string(&mut uefi, "MemoryAllocationFailed\r\n");
+            return uefi::EfiStatus::load_error();
+        },
+        Err(uefi::UEFIError::MemoryMapUnavailable) => {
+            uefi::Console::write_string(&mut uefi, "MemoryMapUnavailable\r\n");
+            return uefi::EfiStatus::load_error();
+        },
+    };
 
     uefi::EfiStatus::success()
 }
